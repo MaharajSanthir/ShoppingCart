e822bbbc2d9e170fec06f581ee893f2e9323604c
Maharaj Santhir <msanthir@gmail.com>
2014-12-03 15:07:27 -0500

    added a product class

---
+++ ShoppingCart/App_Code/product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class product
+    {
+        public int product_id { get; set; }
+        public int cat_id { get; set; }
+        public string name { get; set; }
+        public string  description { get; set; }
+        public decimal price { get; set; }
+    }
--- ShoppingCart/products.aspx.cs
+++ ShoppingCart/products.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace ShoppingCart
 {
@@ -14,8 +16,33 @@ namespace ShoppingCart
             string cat_id = Request.QueryString["cat_id"];
             if (cat_id != null)
             {
+                string connString = ConfigurationManager.ConnectionStrings["ShoppingCartConnectionString"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    string sql = "select * from products where cat_id=" + cat_id;
+                    SqlCommand com = new SqlCommand(sql, conn);
+                    conn.Open();
+                    SqlDataReader reader = com.ExecuteReader();
 
+                    List<product> products = new List<product>();
+                    product product;
+                    while (reader.Read())
+                    {
+                        product = new Product();
+                        product.ID = Convert.ToInt32(reader["product_id"]);
+                        product.Cat_ID = Convert.ToInt32(reader["cat_id"]);
+                        product.Name = reader["name"].ToString();
+                        product.Description = reader["description"].ToString();
+                        product.Price = Convert.ToDecimal(reader["price"]);
+                        products.Add(product);
+                    }
+
+                    GridView1.DataSource = products;
+                    GridView1.DataBind();
+
+                }
 
             }
+        }
     }
 }
